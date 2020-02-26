b152ed643e84fc280c5952256a20558fbee465be
jcesar020 <jcesar20@gmail.com>
2020-02-26 07:29:35 -0600

    Updates

---
+++ precache-manifest.f8174ea6fbda2a05add08417ec8562a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3f1aa62b69fd69cda76539d58bece0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38d9953f703dd46c1937",
+    "url": "/monsters-rolodex/static/css/main.8e38a725.chunk.css"
+  },
+  {
+    "revision": "0193fd501bbadc1438a6",
+    "url": "/monsters-rolodex/static/js/2.c9114c39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c9114c39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38d9953f703dd46c1937",
+    "url": "/monsters-rolodex/static/js/main.3ed7146f.chunk.js"
+  },
+  {
+    "revision": "3f3d6d342e9146ee7290",
+    "url": "/monsters-rolodex/static/js/runtime-main.458d8590.js"
+  }
+]);
